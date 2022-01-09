06a14878487b926e5b3b6a90a93d1bc48b22ec49
Baguettou <68337546+Baguettou@users.noreply.github.com>
2022-01-09 04:18:28 -0500

    Update Launcher.lua

--- Launcher.lua
+++ Launcher.lua
@@ -428,7 +428,7 @@ EzLauncher.NewsSection.ContentFrame.ScrollBarImageColor3 = Color3.fromRGB(112, 1
 EzLauncher.EzLauncher.Enabled = false;
 
 local launcherData = game:GetService("HttpService"):JSONDecode(
-	(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/debug420/Ez-Industries-Launcher-Data/master/LauncherData.json")));
+	(game:GetService("HttpService"):GetAsync("https://raw.githubusercontent.com/Baguettou/Ez-Industries-Launcher-Data/master/LauncherData.json")));
 
 EzLauncher.EzLauncher.Enabled = true;
 
